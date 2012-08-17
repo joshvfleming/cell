8ae0539ac698f4faab8631011b814d68ded3fb43
jfleming <jfleming@symora.com>
2012-08-17 12:39:33 -0700

    Moved js source files to a new js specific folder

---
+++ src/js/Loader.js
@@ -0,0 +1,12 @@
+/* Loader
+ *
+ * This is the file loader class.
+ */
+cell.Loader = (function() {
+  var Loader = function Loader(){};
+
+  Loader.load = function load(path) {
+  };
+
+  return Loader;
+})();
